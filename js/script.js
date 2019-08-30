ab18177787824200f1d9687e781afec9c549391f
Misha <anventahy@mail.ru>
2019-08-30 22:48:45 +0500

    Lesson 1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 'use strict';
 
+let money, time, appData, question1, question2, bugetDay;
+money = +prompt('Ваш бюджет на месяц? ', '');
+time = prompt('Введите дату в формате YYYY-MM-DD', '');
+bugetDay = money / 30;
+
+question1 = prompt('Введите обязательную статью расходов в этом месяце', '');
+question1 = +prompt('Во сколько обойдется', '');
+
+question2 = prompt('Введите обязательную статью расходов в этом месяце', '');
+question2 = +prompt('Во сколько обойдется', '');
+
+appData = {
+    money,
+    time,
+    expenses: {
+       question1, question2
+    },
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+alert('Ваш бюджет на день составляет: ' + bugetDay.toFixed(2) + ' рублей');
+console.log('appData: ', appData);
